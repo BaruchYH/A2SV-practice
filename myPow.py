ce53a56b9f26489b17ce604d5b07cbd9eba2a0ea
zelnev21 <95002710+zelnev21@users.noreply.github.com>
2022-01-01 05:13:27 -0800

    Create myPow.py

---
+++ myPow.py
@@ -0,0 +1,28 @@
+class Solution:
+    
+    def myPow(self, x: float, n: int) -> float:
+        
+        if n < 0 :
+            return 1/ self.myPow2(x, -n)
+        
+        elif n == 0 :
+            return 1 
+        
+        else :
+            return self.myPow2(x , n )
+        
+        
+        
+    def myPow2(self, x: float, n:int) :
+        
+        if n == 0:
+            return 1
+        
+        elif n%2 == 0 :
+            curr= self.myPow2(x , n//2)
+            
+            return curr*curr
+        
+        else : 
+            curr= self.myPow2(x , n//2)
+            return x*curr*curr
