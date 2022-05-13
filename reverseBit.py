5791387f2a92a0d56b9e225fb23118024a2429a2
zelnev21 <95002710+zelnev21@users.noreply.github.com>
2022-05-12 21:30:19 -0700

    Create reverseBit.py

---
+++ reverseBit.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        
+        n = bin(n)[2:]               
+        n = '%32s' %n       
+        n = n.replace(' ', '0')[:]  
+        
+        return int(n[::-1], 2)
