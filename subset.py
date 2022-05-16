4c447b411c0fc151c1194432fe3293cc64007721
zelnev21 <95002710+zelnev21@users.noreply.github.com>
2022-05-15 21:46:36 -0700

    Create subset.py

---
+++ subset.py
@@ -0,0 +1,16 @@
+     def subsets(self, nums: List[int]) -> List[List[int]]:
+        
+#         def sub_encoding(s):
+            
+#             return [nums[i] for i in range(len(nums)) if s[i] == '1']
+        
+#         res = []
+#         for i in range (2**len(nums)):
+            
+#             L = len(bin(2**len(nums)-1)[2:])
+#             s = bin(i)[2:]
+#             s = '0'*(L-len(s)) + s
+            
+#             res.append(sub_encoding(s))
+            
+#         return res 
