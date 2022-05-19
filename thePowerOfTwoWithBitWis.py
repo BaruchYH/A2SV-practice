c2e04c427a36e22e14385821472dde10e658e81e
zelnev21 <95002710+zelnev21@users.noreply.github.com>
2022-05-19 14:26:02 -0700

    Create thePowerOfTwoWithBitWis.py

---
+++ thePowerOfTwoWithBitWis.py
@@ -0,0 +1,9 @@
+
+
+            start = 1
+            while n > start:
+                start = start*2
+                
+            if n == start:
+                return True
+            return False 
