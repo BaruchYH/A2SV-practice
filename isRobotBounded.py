79700282d65bea751a8740afe5f2f12c8a4791fa
zelnev21 <95002710+zelnev21@users.noreply.github.com>
2023-02-07 23:41:59 +0300

    Create isRobotBounded.py

---
+++ isRobotBounded.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isRobotBounded(self, instructions: str) -> bool:
+        
+        pos = [0, 0]
+        direction = 0
+        
+        for inst in instructions:
+            if inst == 'G':
+                if direction==0:
+                    pos[1] += 1
+                elif direction == 2:
+                    pos[1] -= 1
+                elif direction == 1:
+                    pos[0] += 1
+                else:
+                    pos[0] -= 1
+            elif inst == 'L':
+                direction = (direction-1)%4
+            elif inst == 'R':
+                direction = (direction+1)%4
+        
+        if direction!=0 or pos==[0,0]:
+            return True
+        else:
+            return False
+                
+        
